d6853e67f0be325f1036b7277ec6b305064e815f
lucifer <fortran95@126.com>
2012-08-06 18:19:44 +0800

    Receipting mechanism added.

--- widgets/dialogbox.py
+++ widgets/dialogbox.py
@@ -3,17 +3,36 @@
 # 显示聊天内容的对话框
 
 from Tkinter import *
+import hashlib
 
 class DialogBox(Text):
+    require_receipt = True
+
     def __init__(self,master,**options):
         Text.__init__(self,master,**options)
         self.config(padx=7,pady=5)
 
-    def newrec(self,headline,text):
-        pass
-    def _append(self,tag, content, fg_color='black', bg_color='white', lmargin=0):
+    def newrecord(self,headline,text,is_ours):
+        tagbase = hashlib.md5(headline + text).hexdigest()
+        if is_ours:
+            fgc,bgc1 = '#00A','white'
+            if self.require_receipt:
+                bgc2 = '#FCC'
+            else:
+                bgc2 = 'white'
+        else:
+            fgc,bgc1,bgc2 = '#A00','white','white'
+        self._append(tagbase + '/headline',headline + '\n',fgc,bgc1,0,10,'Sans 9 bold')
+        self._append(tagbase + '/content' ,text + '\n','black',bgc2,15,10,'Sans 9')
+
+        return tagbase
+
+    def mark_received(self,tag):
+        self.tag_config(tag + '/content',background='white')
+
+    def _append(self,tag, content, fg_color, bg_color, lmargin, offset, font):
         self.config(state=NORMAL)
-	self.tag_config(tag, foreground=fg_color, background=bg_color, lmargin1=lmargin,lmargin2=lmargin)
+	self.tag_config(tag, foreground=fg_color, background=bg_color, lmargin1=lmargin,lmargin2=lmargin, offset=offset, font=font)
 	self.insert(END,content,tag)
 	self.yview(END)
 	self.config(state=DISABLED) 
@@ -22,6 +41,9 @@ if __name__ == '__main__':
     root = Tk()
     box = DialogBox(root)
     box.pack()
-    box._append('tag1','This is some content\n','#A00','white',0)
-    box._append('tag2','Yes' * 100 + '\n','#000','white',10)
+    box.require_receipt = False
+    box.newrecord('HMX (2012-12-21 05:30:29):','大叔……',False)
+    receipt = box.newrecord('Whirlpool (2012-12-21 05:31:01):','哈' * 50 + '.' * 102,True)
+#    box.mark_received(receipt)
+    box.newrecord('HMX (2012-12-21 05:32:00):','= =......',False)
     root.mainloop()
