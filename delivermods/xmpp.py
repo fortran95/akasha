e738d86f39fb9d613534eba3e5dfc9ba98a0eaf0
lucifer <fortran95@126.com>
2012-08-30 22:47:59 +0800

    Doing xmpp deliver module.

--- delivermods/xmpp.py
+++ delivermods/xmpp.py
@@ -1,4 +1,22 @@
 import sleekxmpp
 
 class XMPP(object):
-    pass
+    def __init__(self,jid,password):
+        self.xmpp = sleekxmpp.ClientXMPP(jid,password)
+
+        self.xmpp.add_event_handler("session_start",self._onConnected)
+        self.xmpp.add_event_handler("message",self._onMessage)
+        self.xmpp.add_event_handler("disconnected",self._onDisconnected)
+
+    def run(self):
+        self.xmpp.connect()
+        self.xmpp.process(block=False)
+
+    def _onConnected(self,event):
+        self.xmpp.sendPresence()
+
+    def _onDisconnected(self,event):
+        pass
+
+    def _onMessage(self,message):
+        pass
