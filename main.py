8035c3bda1ce7fa50694d2a8e827533501264abb
lucifer <fortran95@126.com>
2012-08-31 16:12:16 +0800

    Still adjusting CLI

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class Screen(object):
                    ('pads.available', 'white', 'dark blue'),
                    ('pads.unavailable', 'black', 'light gray'),
                    ('default', 'white', 'dark blue'),
+                   ('area.focused', 'white', 'dark blue'),
+                   ('area.notfocused','dark blue','dark blue'),
                   ]
 
         self._createWidgets()
@@ -17,7 +19,7 @@ class Screen(object):
             if input in 'abcABC':
                 self._accountScreen[ord(input)-ord('a')].set_attr_map({None:'pads.available'})
             if input in '0123456789':
-                self._accountLights['neoatlantis@pidgin.su'][ord(input) - ord('0')].set_attr_map({None:'lights.connecting'})
+                self._accountLights['neoatlantis@pidgin.su'][ord(input) - ord('0')].set_attr_map({None:'lights.available'})
             if input in ('q','Q'):
                 raise urwid.ExitMainLoop()
         urwid.MainLoop(self.main,palette,unhandled_input=handler).run()
@@ -30,7 +32,7 @@ class Screen(object):
             buddies = range(0,30)
             
             for buddy in buddies:
-                light = urwid.AttrMap(urwid.Text(str(buddy)),'lights.unavailable')
+                light = urwid.AttrMap(urwid.Text(u'第 %d 号' % buddy),'lights.unavailable')
                 self._accountLights[account].append(light)
 
             pad = urwid.LineBox(urwid.GridFlow(self._accountLights[account],
@@ -42,19 +44,27 @@ class Screen(object):
         return self._accountScreen
     def _getEntities(self):
         self._entities = []
-        return self._entities()
+        for i in range(0,100):
+            self._entities.append(urwid.Text(u"用户实体 %d" % i))
+        return self._entities
 
     def _createWidgets(self):
         self.statusListWalker = urwid.SimpleListWalker(self._getAccountScreens())
         self.statusList = urwid.ListBox(self.statusListWalker)
 
-        self.left  = urwid.BoxAdapter(self.statusList,60)
-        self.right = urwid.AttrMap(
-                                   urwid.LineBox(
-                                        urwid.Text("Help:\n F1-Exit F2-Enter")),
-                                   'default')
+        self.left  = urwid.BoxAdapter(self.statusList,40)
+        self.leftFocusIndicator = urwid.AttrMap(self.left,'area.focused')
+
+        self.entityListWalker = urwid.SimpleListWalker(self._getEntities())
+        self.entityList = urwid.BoxAdapter(urwid.ListBox(self.entityListWalker),20)
+
+        self.right_helpbox = urwid.AttrMap(urwid.LineBox(
+                                                urwid.Text("Help:\n F1-Exit F2-Enter")),
+                                           'default')
+        self.right = urwid.Pile([self.right_helpbox,self.entityList])
+
         
-        self.cols = urwid.Columns([self.left,
+        self.cols = urwid.Columns([self.leftFocusIndicator,
                                    ('fixed',32,self.right)]
                                  ,1)
 
